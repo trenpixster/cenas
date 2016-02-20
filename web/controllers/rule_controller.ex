1e21628ae9e0ea47b04d24c293839c457df35a33
trenpixster <nizar.venturini@gmail.com>
2016-02-20 23:18:45 +0000

    Add delete click logic

--- web/controllers/rule_controller.ex
+++ web/controllers/rule_controller.ex
@@ -8,9 +8,12 @@ defmodule Hackathon.RuleController do
     trigger = params["trigger"]
     action = params["action"]
     nfa_id = params["nfa_id"]
+    click_id = params["click_id"]
 
     model = %Rule{nfa_id: nfa_id, trigger: trigger, action: action, title: title}
     Repo.insert model
+
+    Repo.delete Click, id: click_id
 
     json conn, (model |> Map.drop [:__meta__, :__struct__])
   end
