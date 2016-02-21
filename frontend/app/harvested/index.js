156522e169f92a47d1115f27e3a53c9894c994f3
Pedro Sousa <pedro.cruz.sousa@gmail.com>
2016-02-21 11:23:32 +0000

    Unignore support

--- frontend/app/harvested/index.js
+++ frontend/app/harvested/index.js
@@ -11,38 +11,41 @@
     function fetchData () {
         const url = isHarvest ? '/clicks' : '/clicks/ignored',
             title = isHarvest ? 'Harvested Clicks' : 'Ignored Clicks';
+
         $.get(url).then((clicks) => {
             $.content.find('.title').text(title);
-            $.content.find('.card-container').html(cardsTemplate({ clicks }))
+            $.content.find('.card-container').html(cardsTemplate({ clicks, isHarvest }))
         });
-        $('.js-ignore-click').on('click', (ev) => {
+    }
+
+    function setup () {
+        cards.init();
+        $(document).on('change', 'input[type="checkbox"]', () => {
+            isHarvest = !isHarvest;
+            fetchData();
+        });
+
+        $(document).on('click', '.js-ignore-click', (ev) => {
             ev.preventDefault();
-            const clickId = $(ev.target).attr('data-click-id');
+            const clickId = $(ev.target).data('click-id');
             $(ev.target).addClass('disabled');
-            $.post('/click/ignore', { click_id: clickId })
-             .then(() => {
+            $.post('/click/ignore', { click_id: clickId }).then(() => {
                   $(ev.target).removeClass('disabled');
                   page.redirect('/dashboard/harvested');
             });
         });
     }
 
-    function setup () {
-        cards.init();
-        $.content.find('input[type="checkbox"]').on('change', () => {
-            isHarvest = !isHarvest;
-            fetchData();
-        });
-    }
-
     module.exports = {
         enter (ctx) {
-            $.content.html(template({ clicks: ctx.clicks }));
+            $.content.html(template({ clicks: ctx.clicks, isHarvest }));
             setup();
         },
 
         exit (ctx, next) {
             cards.destroy();
+            $(document).off('click', '.js-ignore-click');
+            $(document).off('change', 'input[type="checkbox"]');
             next();
         }
     };
