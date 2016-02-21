58e7faf34396aaeeb86254f2db4e04849de19b55
trenpixster <nizar.venturini@gmail.com>
2016-02-21 20:03:47 +0000

    Add logger to google analytics stuff

--- lib/hackathon/interactors/send_data_to_google_analytics_interactor.ex
+++ lib/hackathon/interactors/send_data_to_google_analytics_interactor.ex
@@ -1,11 +1,12 @@
 defmodule Hackathon.SendDataToGoogleAnalyticsInteractor do
+  require Logger
   def call(v, ec, ea, el, ev, cid, tid) do
     body = gen_body(v, ec, ea, el, ev, cid, tid)
     HTTPotion.post "https://www.google-analytics.com/collect", [body: body, headers: ["User-Agent": "Hackathon Elixir"]]
   end
 
   def gen_body(v, ec, ea, el, ev, cid, tid) do
-    IO.puts "v=#{v}&ec=#{ec}&ea=#{ea}&el=#{el}&ev=#{ev}&cid=#{cid}&tid=#{tid}&t=event"
+    Logger.info "v=#{v}&ec=#{ec}&ea=#{ea}&el=#{el}&ev=#{ev}&cid=#{cid}&tid=#{tid}&t=event"
     "v=#{v}&ec=#{ec}&ea=#{ea}&el=#{el}&ev=#{ev}&cid=#{cid}&tid=#{tid}&t=event"
   end
 end
