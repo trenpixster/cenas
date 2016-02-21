c46d7a66e0ef03862481bf6ed90360170aa81fcc
trenpixster <nizar.venturini@gmail.com>
2016-02-21 22:49:58 +0000

    revert stuff

--- web/controllers/click_controller.ex
+++ web/controllers/click_controller.ex
@@ -55,11 +55,6 @@ defmodule Hackathon.ClickController do
   end
 
   def click(conn, params) do
-    spawn __MODULE__, :process_click, [params]
-    json conn, %{success: true}
-  end
-
-  def process_click(params) do
     cid = params["cid"]
     nfa_id = params["nfa_id"]
     is_bookmarklet = params["bookmarklet"]
@@ -67,6 +62,7 @@ defmodule Hackathon.ClickController do
     payload = params["payload"]
 
     insert_click(cid, nfa_id, url, is_bookmarklet, payload)
+    json conn, %{success: true}
   end
 
   defp insert_click(cid, nfa_id, url, is_bookmarklet, payload) do
