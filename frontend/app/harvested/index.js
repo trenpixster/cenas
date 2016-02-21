83d30c45961ac852dce2557cce2b227d09dd9e08
Pedro Sousa <pedro.cruz.sousa@gmail.com>
2016-02-21 12:19:27 +0000

    Support ignore and unignore

--- frontend/app/harvested/index.js
+++ frontend/app/harvested/index.js
@@ -1,51 +1,62 @@
 (function ($) {
     'use strict';
-    const page = require('page');
 
-    const template = require('app/templates/harvested.hbs'),
-        cardsTemplate = require('app/templates/partials/harvested-cards.hbs'),
+    const page = require('page'),
+        template = require('app/templates/harvested.hbs'),
         cards = require('app/helpers/cards');
 
-    let isHarvest = true;
+    let isHarvest;
 
-    function fetchData () {
-        const url = isHarvest ? '/clicks' : '/clicks/ignored',
-            title = isHarvest ? 'Harvested Clicks' : 'Ignored Clicks';
-
-        $.get(url).then((clicks) => {
-            $.content.find('.title').text(title);
-            $.content.find('.card-container').html(cardsTemplate({ clicks, isHarvest }))
-        });
-    }
+    const getTitle = () => isHarvest ? 'Harvested Clicks' : 'Ignored Clicks';
 
     function setup () {
         cards.init();
-        $(document).on('change', 'input[type="checkbox"]', () => {
+        $.content.find('input[type="checkbox"]').on('change', () => {
             isHarvest = !isHarvest;
-            fetchData();
+            page.redirect(`/dashboard/harvested?view=${isHarvest ? 'harvested' : 'ignored'}`);
         });
 
-        $(document).on('click', '.js-ignore-click', (ev) => {
+        $.content.find('[data-ignore]').on('click', (ev) => {
             ev.preventDefault();
-            const clickId = $(ev.target).data('click-id');
-            $(ev.target).addClass('disabled');
+            const $target = $(ev.target),
+                clickId = $target.data('ignore');
+            $target.addClass('disabled');
             $.post('/click/ignore', { click_id: clickId }).then(() => {
-                  $(ev.target).removeClass('disabled');
-                  page.redirect('/dashboard/harvested');
+                $target.removeClass('disabled');
+                page.redirect('/dashboard/harvested');
             });
+        });
+
+        $.content.find('[data-unignore]').on('click', (ev) => {
+            ev.preventDefault();
+            const $target = $(ev.target),
+                clickId = $(ev.target).data('unignore');
+            $target.addClass('disabled');
+            $.ajax({
+                url:         '/click/ignore',
+                method:      'delete',
+                data:        JSON.stringify({ click_id: clickId }),
+                contentType: 'application/json',
+                dataType:    'json'
+            }).then(() => {
+                $target.removeClass('disabled');
+                page.redirect('/dashboard/harvested?view=ignored');
+            })
         });
     }
 
     module.exports = {
         enter (ctx) {
-            $.content.html(template({ clicks: ctx.clicks, isHarvest }));
+            isHarvest = ctx.query.view !== 'ignored';
+            $.content.html(template({ clicks: ctx.clicks, isHarvest, title: getTitle() }));
             setup();
         },
 
         exit (ctx, next) {
             cards.destroy();
-            $(document).off('click', '.js-ignore-click');
-            $(document).off('change', 'input[type="checkbox"]');
+            $.content.find('[data-ignore]').off('click');
+            $.content.find('[data-unignore]').off('click');
+            $.content.find('input[type="checkbox"]').off('change');
             next();
         }
     };
--- frontend/app/middlewares/harvested/get-harvested.js
+++ frontend/app/middlewares/harvested/get-harvested.js
@@ -3,9 +3,11 @@
 
     const state = require('app/state');
     module.exports = function getHarvested (ctx, next) {
-        const $card = $.content.find(`.card[data-harvested]`);
+        const $card = $.content.find(`.card[data-harvested]`),
+            url = ctx.query.view === 'ignored' ? '/clicks/ignored' : '/clicks';
+
         $card.addClass('loading');
-        $.get('/clicks').then((clicks) => {
+        $.get(url).then((clicks) => {
             ctx.clicks = clicks;
             state.save('clicks', clicks);
             $card.removeClass('loading');
